1e2adcd73f4fa990b1430fa958665de6f04539f9
Okan Sercan <okansercan@outlook.com>
2023-03-24 00:00:20 +0300

    wallet.api added

---
+++ Wallet.API/Models/Campaign.cs
@@ -0,0 +1,15 @@
+﻿namespace Wallet.API
+{
+    public class Campaign
+    {
+        public string Brand { get; set; }
+        public string Sector { get; set; }
+        public string LastDate { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string DetailUrl { get; set; }
+        public string Logo { get; set; }
+    }
+}
+
---
+++ Wallet.API/Models/Sector.cs
@@ -0,0 +1,10 @@
+﻿namespace Wallet.API
+{
+	public class Sector
+	{
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Link { get; set; }
+    }
+}
+
